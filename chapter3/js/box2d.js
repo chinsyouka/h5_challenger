eb42c3f62b0588a136d4709ab649ce714938b217
chinsyouka <chinsyouka@163.com>
2015-08-03 03:07:25 +0800

    box2d初步

---
+++ chapter3/js/box2d.js
@@ -0,0 +1,12 @@
+var b2Vec2 = Box2D.Common.Math.b2Vec2();
+var b2BodyDef = Box2D.Dynamics.b2BodyDef();
+var b2Body = Box2D.Dynamics.b2Body();
+
+var Box2D = {
+	
+	
+}
+
+
+$(document).ready(function(){
+});
